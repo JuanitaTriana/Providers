9cdc07b7c80b1aa03d3fc82d0a0c3603cb3797ff
juanmorenosundevs <juan.moreno@sundevs.com>
2020-05-30 12:25:28 -0500

    Add nav-bar to shared components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { InventoryTableComponent } from './inventory-table/inventory-table.compo
 import { AddProviderComponent } from './add-provider/add-provider.component';
 import { AddSubsidiaryComponent } from './add-subsidiary/add-subsidiary.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
     InventoryTableComponent,
     AddProviderComponent,
     AddSubsidiaryComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
