9d6a9e9215066999b6250c6d2bebbf9545e79a82
Juanita Triana <juanitatriana@Juanitas-MBP.lan>
2020-06-03 22:10:06 -0500

    hide the search box

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  isHome: boolean = true;
 
   constructor(private router: Router) { }
   ngOnInit(): void {
