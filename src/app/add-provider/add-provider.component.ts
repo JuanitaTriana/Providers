196b27543046f4cd208fb696311d44197e35d00c
Juanita Triana <juanitatriana@Juanitas-MBP.lan>
2020-06-05 22:35:29 -0500

    form fields added

--- src/app/add-provider/add-provider.component.ts
+++ src/app/add-provider/add-provider.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-add-provider',
   templateUrl: './add-provider.component.html',
   styleUrls: ['./add-provider.component.css']
 })
+
 export class AddProviderComponent implements OnInit {
 
   constructor() { }
