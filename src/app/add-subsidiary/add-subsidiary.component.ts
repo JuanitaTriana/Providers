8044135474c0ad4d74cf35cb3ebfa28426498857
Juanita Triana <juanitatriana@Juanitas-MBP.lan>
2020-06-07 17:31:23 -0500

    agregar la pantalla de sucursal y el desplegable

--- src/app/add-subsidiary/add-subsidiary.component.ts
+++ src/app/add-subsidiary/add-subsidiary.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-subsidiary',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-subsidiary.component.css']
 })
 export class AddSubsidiaryComponent implements OnInit {
+  panelOpenState = true;
+
+  public name: string;
+  public email: string;
+  public address: string;
+  public addre: string;
+  public phone: string;
+  public state: string;
+  public city: string;
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  nameFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  addressFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  phoneFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  stateFormControl = new FormControl('', [
+    Validators.required,
+  ])
+  cityFormControl = new FormControl('', [
+    Validators.required,
+  ])
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  saveProvider(): void {
+    if (this.nameFormControl.valid && this.emailFormControl.valid && this.addressFormControl.valid && this.phoneFormControl.valid){
+      alert('Proveedor Guardado'+'\n'+ 'provider info: '+'\n'+this.name +'\n'+ this.email + this.address + '\n'+ this.addre +'\n'+this.phone) 
+    }
+  }
 }
