113ba6893ffc5fc245dfdeb8734d8fb33a8ab67f
unknown <ces0708sar@hotmail.com>
2020-06-10 19:17:24 -0500

    add method to at products cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AddProviderComponent } from './add-provider/add-provider.component';
 import { AddSubsidiaryComponent } from './add-subsidiary/add-subsidiary.component';
 import { ProvidersListComponent } from './providers-list/providers-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'providers-list',
     component: ProvidersListComponent
+  },
+  {
+    path: 'shopping-cart',
+    component: ShoppingCartComponent
   }
 ];
 
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
-export class ShoppingCartComponent implements OnInit {
+export class ShoppingCartComponent implements OnInit{
+  panelOpenState = false;
 
-  constructor() { }
+    entrada = {
+      name: 'Proveedor D', 
+      products: [{name: 'product4', serial: '123'}]
+    }
 
-  ngOnInit(): void {
-  }
+  arrayCarrito = [
+    {
+      name: 'Proveedor C', 
+      products: [
+        {name: 'product1', serial: '123'},
+        {name: 'product2', serial: '1123'},
+        {name: 'product3', serial: '1123'}
+      ]
+    },
+    {
+      name: 'Proveedor B', 
+      products: [
+        {name: 'product1', serial: '123'},
+        {name: 'product2', serial: '1123'}
+      ]
+    }];
+
+    ngOnInit(){
+      this.addToCart(this.entrada)
+    }
+
+    addToCart(entrada){
+      console.log(entrada)
+      const found = this.arrayCarrito.find(element => element.name === entrada.name);
+      console.log(found);
+
+      if (found != undefined){
+        entrada.products.forEach(product => {
+          found.products.push(product)
+          
+        });
+        
+      }else{
+        this.arrayCarrito.push(entrada)
+
+      }
+
+      console.log(this.arrayCarrito)
+    }
 
 }
